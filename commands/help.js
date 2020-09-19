75a77e0951e45e24aff3c2a8af0717b09ceab6a9
Fadil Ardiansyah <v@clude.id>
2020-09-19 12:46:21 +0700

    swap help to in channel instead DMs, add enemy commands

--- commands/help.js
+++ commands/help.js
@@ -1,47 +1,38 @@
+const { settings } = require("cluster");
+const fs = require("fs");
+const bot_config = require('../bot.config.json');
+const prefix = bot_config.bot_config.prefix;
+let streng = [];
+fs.readdir("./commands/", (err, files) => {
+    if(err) console.error(err);
+
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <= 0) {
+        console.log("No commands to load!");
+        return;
+    }
+
+  
+    let result = jsfiles.forEach((f, i) => {
+        let props = require(`./${f}`);
+        let filesArray = {name: prefix+' '+props.name, value: props.description};
+        streng.push(filesArray);
+        
+    });
+ 
+
+});
 module.exports = {
     name: 'help',
-    description: 'List all of my commands or info about a specific command.',
-    aliases: ['commands'],
-    usage: '[command name]',
-    cooldown: 5,
-    execute(message, args) {
-        const config  = require('../bot.config.json');
-        const prefix = config.bot_config.prefix;
+    description: 'Get information about kurebot commands',
+    execute(message, args, client) {
+        const Discord = require('discord.js');
+        const emsg = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Kurebot Command list')
+            .addFields(streng)
+        
+        message.channel.send(emsg);
 
-        const data = [];
-        const { commands } = message.client;
-
-        if (!args.length) {
-            data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-
-            return message.author.send(data, { split: true })
-                .then(() => {
-                    if (message.channel.type === 'dm') return;
-                    message.reply('I\'ve sent you a DM with all my commands!');
-                })
-                .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-                });
-        }
-
-        const name = args[0].toLowerCase();
-        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-        if (!command) {
-            return message.reply('that\'s not a valid command!');
-        }
-
-        data.push(`**Name:** ${command.name}`);
-
-        if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-        message.channel.send(data, { split: true });
-
-    },
-};
+    }
+}
